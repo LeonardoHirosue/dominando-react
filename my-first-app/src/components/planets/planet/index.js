1bce15179f6acd7bfb2d4bd14aa1da80d0ac6b13
Leonardo Naoto Hirosue <leonardo.hirosue@gmail.com>
2022-08-26 15:31:47 -0300

    Conhecendo as props

--- my-first-app/src/components/planets/planet/index.js
+++ my-first-app/src/components/planets/planet/index.js
@@ -1,11 +1,11 @@
 import GrayImg from '../../shared/gray_img'
 
-const Planet = () => {
+const Planet = (props) => {
     return (
         <div>
-            <h4>Mercúrio</h4>
-            <p>Mercúrio é o menor[nota 1][nota 2] e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em relação ao plano da órbita dentre todos os planetas do Sistema Solar. Mercúrio completa três rotações em torno de seu eixo a cada duas órbitas.</p>
-            <GrayImg/>
+            <h4>{props.title}</h4>
+            <p>{props.description}</p>
+            <GrayImg url_img = {props.img_url}/>
         </div>
     )
 }
