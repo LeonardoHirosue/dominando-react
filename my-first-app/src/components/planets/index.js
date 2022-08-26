34d45283130c61038cb941bf79ecc2c3e405ad31
Leonardo Naoto Hirosue <leonardo.hirosue@gmail.com>
2022-08-26 17:16:09 -0300

    Reenderizando múltiplos components(Satelites)

--- my-first-app/src/components/planets/index.js
+++ my-first-app/src/components/planets/index.js
@@ -7,7 +7,6 @@ const Planets = () =>{
             <h3>Planet List</h3>
             <hr/>
             <Planet title = "Mercúrio"
-                    description = "Mercúrio é o menor[nota 1][nota 2] e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em relação ao plano da órbita dentre todos os planetas do Sistema Solar. Mercúrio completa três rotações em torno de seu eixo a cada duas órbitas."
                     img_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/420px-Mercury_in_color_-_Prockter07-edit1.jpg"
                     link = "https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
                     title_with_underline = {true}
--- my-first-app/src/components/planets/planet/index.js
+++ my-first-app/src/components/planets/planet/index.js
@@ -2,6 +2,9 @@ import DescriptionWithLink from '../../shared/description_with_link'
 import GrayImg from '../../shared/gray_img'
 
 const Planet = (props) => {
+    let names = ['a', 'b', 'c', 'd', 'e']
+    let satellites = names.map(name => <li>Satélite {name}</li>)
+    
     let title;
     if(props.title_with_underline){
         title =  <h4><u>{props.title}</u></h4>
@@ -14,6 +17,12 @@ const Planet = (props) => {
             {title}
             <DescriptionWithLink description = {props.description} link = {props.link}/>
             <GrayImg url_img = {props.img_url} color = {props.color}/>
+
+            <h4>Satélites</h4>
+            <ul>
+                {satellites}
+            </ul>
+            <hr/>
         </div>
     )
 }
