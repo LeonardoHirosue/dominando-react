ec04c438abebfc7e1f90c7c3a42707c5021baca4
Leonardo Naoto Hirosue <leonardo.hirosue@gmail.com>
2022-08-27 17:01:26 -0300

    Refatorando lifecycle com hooks no component Planets

--- my-first-app/src/components/planets/index.js
+++ my-first-app/src/components/planets/index.js
@@ -1,29 +1,20 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import Planet from './planet'
 
-// async function getPlanets(){
-//     const response = await fetch('http://localhost:3000/api/planets.json');
-//     const data = response.json();
-//     return data;
-// }
+async function getPlanets(){
+    const response = await fetch('http://localhost:3000/api/planets.json');
+    const data = response.json();
+    return data;
+}
 
 const Planets = () => {
-    const[ planets, setPlanets] = useState([
-        {
-            "id": "mars",
-            "name": "Mars",
-            "description": "Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System after Mercury. In English, Mars carries a name of the Roman god of war and is often referred to as the 'Red Planet'.",
-            "img_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/220px-OSIRIS_Mars_true_color.jpg",
-            "link": "https://en.wikipedia.org/wiki/Mars"
-        }
-    ])
-    // componentDidMount(){
-    //     getPlanets().then(data =>{
-    //         this.setState(state => ({
-    //             planets: data['planets']
-    //         }))
-    //     })
-    // }
+    const[ planets, setPlanets] = useState([])
+
+    useEffect(() => {
+        getPlanets().then(data =>{
+            setPlanets(data['planets'])
+        })
+    }, [])
 
     const duplicateLast = () => {
         let lastPlanet = planets[planets.length-1]
